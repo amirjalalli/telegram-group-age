1bc034525d34325427714423ba44174f43c23ef5
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-30 20:46:56 +0330

    telegram-group-age

--- js.js
+++ js.js
@@ -1,8 +1,26 @@
-var ages = [19, 20 , 22 , 23 ,18]
+var groupUsers = [
+    {id :1 , name : "ali", age : 19},
+    {id :2 , name : "amir", age : 22},
+    {id :3 , name : "amin", age : 21},
+    {id :4 , name : "mat", age : 25},
+    {id :5 , name : "ari", age : 24},
+    {id :6 , name : "mohammd", age : 25},
+    {id :7 , name : "asghar", age : 26},
+    {id :8 , name : "reze", age : 27},
+    {id :9 , name : "essi", age : 28},
+    {id :10 , name : "mahnaz", age : 29},
+    {id :11 , name : "mona", age : 30},
+]
 
-var isAll = ages.every(function(age){
-    console.log(age)
-    return age > 18
+var isAll = groupUsers.every(function(user){
+    
+    return user.age > 18
 })
 
-console.log(isAll)
+if ( isAll == true ) {
+    console.log(" شما می توانید تماس دریافت کنید")
+} else {
+    console.log(
+    "شما نمیتوانید تماس بگیرید"
+    )
+}
